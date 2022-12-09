d65a9fc663583fc605c88c1775734a44ae98a41f
nesslei <botchedbunnies@gmail.com>
2022-12-09 13:57:11 -0500

    awvadvwva
    awvdv

--- vite-slay/main.js
+++ vite-slay/main.js
@@ -2,6 +2,8 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
 
+const poop = { display: document.querySelector(".hedgehogs") };
+
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -26,21 +28,24 @@ const hedgehogs = [
   {
     id: 1,
     name: `hedgy1`,
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS23kXPcP0PHe9pr9H6qW59vMT3P8G8fzRJ4g&usqp=CAU",
   },
   {
     id: 2,
     name: `hedgy2`,
+    url: "https://i.pinimg.com/736x/98/ba/48/98ba48c230f378e064a02ec15c3b7227.jpg",
   },
   {
     id: 3,
     name: `hedgy3`,
-    url: 
+    url: "https://i.pinimg.com/originals/87/a9/65/87a9655bf12a64432d0d0bca90dd2887.jpg",
   },
   {
     id: 4,
     name: `hedgy4`,
     url: "https://goldenexoticpets.com/wp-content/uploads/2020/06/Buy-Albino-Pygmy-Hedgehog.jpg",
   },
+
   {
     id: 5,
     name: `hedgy5`,
