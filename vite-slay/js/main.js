582b8fa8780e1abddc84128c5db6dc47299bf3d8
nesslei <botchedbunnies@gmail.com>
2022-12-13 13:50:51 -0500

    wevawevawvea
    wavevweav

--- vite-slay/js/main.js
+++ vite-slay/js/main.js
@@ -24,35 +24,36 @@ const hedgehogs = [
   {
     id: 1,
     name: `hedgy1`,
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS23kXPcP0PHe9pr9H6qW59vMT3P8G8fzRJ4g&usqp=CAU",
-    color: 
+    url: "",
+    color: "",
   },
+
   {
     id: 2,
     name: `hedgy2`,
-    url: "https://i.pinimg.com/736x/98/ba/48/98ba48c230f378e064a02ec15c3b7227.jpg",
+    url: "",
   },
   {
     id: 3,
     name: `hedgy3`,
-    url: "https://i.pinimg.com/originals/87/a9/65/87a9655bf12a64432d0d0bca90dd2887.jpg",
+    url: "",
   },
   {
     id: 4,
     name: `hedgy4`,
-    url: "https://goldenexoticpets.com/wp-content/uploads/2020/06/Buy-Albino-Pygmy-Hedgehog.jpg",
+    url: "",
   },
 
   {
     id: 5,
     name: `hedgy5`,
-    url: "https://i.pinimg.com/originals/89/55/a7/8955a71079b6edb4e682eb5d761e4076.jpg",
+    url: "",
   },
 
   {
     id: 6,
     name: `hedgy6`,
-    url: "https://nypost.com/wp-content/uploads/sites/2/2021/05/hedgehog.jpg?quality=75&strip=all&w=1024",
+    url: "",
   },
 
   {
