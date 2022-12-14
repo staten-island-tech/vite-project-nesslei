46800e176dadf31f2f070f3cce4c0ee4a955921d
nesslei <botchedbunnies@gmail.com>
2022-12-14 13:59:13 -0500

    evawveavw
    waweavw

--- vite-slay/js/main.js
+++ vite-slay/js/main.js
@@ -1,5 +1,11 @@
 import "../styles/style.css";
 
+const DOMSelectors = {
+  container: document.querySelectorAll(`#container`),
+  container: document.getElementById("container"),
+  form: document.getElementById("form"),
+};
+
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -23,42 +29,56 @@ setupCounter(document.querySelector("#counter"));
 const hedgehogs = [
   {
     id: 1,
-    name: `hedgy1`,
-    url: "",
-    color: "",
+    name: `Bangalore`,
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-bangalore-xl.jpg.adapt.320w.jpg",
+    classified: "Assault",
   },
-
   {
     id: 2,
-    name: `hedgy2`,
-    url: "",
+    name: `Revenant`,
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-revenant-xl.jpg.adapt.320w.jpg",
+    classified: "Assault",
   },
   {
     id: 3,
-    name: `hedgy3`,
-    url: "",
+    name: "Wraith",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-wraith-xl.jpg.adapt.320w.jpg",
+    classified: "Runner",
   },
   {
     id: 4,
-    name: `hedgy4`,
-    url: "",
+    name: "Pathfinder",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-pathfinder-xl.jpg.adapt.320w.jpg",
+    classified: "Runner",
   },
-
   {
     id: 5,
-    name: `hedgy5`,
-    url: "",
+    name: "Octane",
+    url: "https://static.wikia.nocookie.net/apex-legends/images/f/fd/Octane.png/revision/latest?cb=20201103232014&path-prefix=pl",
+    classified: "Runner",
   },
-
   {
     id: 6,
-    name: `hedgy6`,
-    url: "",
+    name: "Horizon",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-horizon-xl.jpg.adapt.320w.jpg",
+    classified: "Runner",
   },
-
   {
     id: 7,
-    name: `hedgy7`,
-    url: "",
+    name: "Bloodhound",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-bloodhound-xl.jpg.adapt.320w.jpg",
+    classified: "Reacon",
+  },
+  {
+    id: 8,
+    name: "Loba",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-loba-xl.jpg.adapt.320w.jpg",
+    classified: "Support",
+  },
+  {
+    id: 9,
+    name: "Wattson",
+    url: "https://media.contentapi.ea.com/content/dam/apex-legends/common/legends/apex-section-bg-legends-wattson-xl.jpg.adapt.320w.jpg",
+    classified: "Pioneer",
   },
 ];
